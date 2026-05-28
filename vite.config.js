b89e124a3f0f4a1533c2260cc181fb70749a0920
Asyary Raihan <asyaryraihan@gmail.com>
2026-05-28 11:03:59 +0700

    Add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+	allowedHosts: ["localhost", process.env.VITE_APP_DOMAIN || "127.0.0.1"],
+  },
 });
