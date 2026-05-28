b8e78ccf59318b80c2ab7ee5b996d05aa0322411
Asyary Raihan <asyaryraihan@gmail.com>
2026-05-28 11:08:09 +0700

    Change allowed hosts implementation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-	allowedHosts: ["localhost", process.env.VITE_APP_DOMAIN || "127.0.0.1"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      allowedHosts: ["localhost", env.VITE_APP_DOMAIN || "127.0.0.1"],
+    },
+  };
 });
